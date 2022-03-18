07be336e48fb48164814e842e6bbed985ca50270
Ivan Monichev <ivan@monichev.ru>
2022-03-19 01:33:41 +0300

    feat: add validation form

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,44 +1,43 @@
 // Показать ошибки валидации.
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, formComponents) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('popup__text-input_type_error');
+  inputElement.classList.add(formComponents.inputErrorClass);
   errorElement.textContent = errorMessage;
 }
 
 // Скрыть ошибки валидации.
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, formComponents) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__text-input_type_error');
+  inputElement.classList.remove(formComponents.inputErrorClass);
   errorElement.textContent = '';
 }
 
 // Проверка поля ввода на ошибки валидации.
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, formComponents) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, formComponents);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, formComponents);
   }
-
 }
 
 // Переключение кнопки из активного состояния в неактивное
-const toggleButtonState = (inputList, buttonElement) => {
+const toggleButtonState = (inputList, buttonElement, formComponents) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('button-inactive');
+    buttonElement.classList.add(formComponents.inactiveButtonClass);
   } else {
-    buttonElement.classList.remove('button-inactive');
+    buttonElement.classList.remove(formComponents.inactiveButtonClass);
   }
 }
 
 // Установка слушателя
-const setEventListenersInputs = (formElement, formComponent) => {
-  const inputList = Array.from(formElement.querySelectorAll(formComponent.inputSelector));
-  const buttonElement = formElement.querySelector(formComponent.submitButtonSelector);
+const setEventListenersInputs = (formElement, formComponents) => {
+  const inputList = Array.from(formElement.querySelectorAll(formComponents.inputSelector));
+  const buttonElement = formElement.querySelector(formComponents.submitButtonSelector);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
+      checkInputValidity(formElement, inputElement, formComponents);
+      toggleButtonState(inputList, buttonElement, formComponents);
     });
   });
 }
@@ -53,7 +52,6 @@ enableValidation = (formComponents) => {
 // Проверка поля ввода на валидацию.
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
-    console.log(!inputElement.validity.valid);
     return !inputElement.validity.valid;
   })
 }
@@ -62,7 +60,7 @@ enableValidation({
   formSelector: '.popup__container',
   inputSelector: '.popup__text-input',
   submitButtonSelector: '.popup__save-button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
+  inactiveButtonClass: 'button-inactive',
+  inputErrorClass: 'popup__input-text_type_error',
   errorClass: 'popup__error_visible'
 });

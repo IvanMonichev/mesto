85a358cc84b2aa4576651ac7577c3f77d7bc854e
Ivan Monichev <ivan@monichev.ru>
2022-05-05 00:43:18 +0300

    fix: refactoring PopupWithImage component

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,12 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._popupImage = this._popupElement.querySelector('.image-zoom__image');
+    this._popupCaption = this._popupElement.querySelector('.image-zoom__caption');
   }
 
   open({name, link}) {
     super.open();
-    this._popupImage = this._popupElement.querySelector('.image-zoom__image');
-    this._popupCaption = this._popupElement.querySelector('.image-zoom__caption');
 
     this._popupImage.src = link;
     this._popupImage.alt = `Фотография загруженная пользователем «${name}»`;

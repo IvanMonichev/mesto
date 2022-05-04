74c245dd2d7fa831ce0d5a427fa605c45c40c8d0
Ivan Monichev <ivan@monichev.ru>
2022-05-04 20:42:19 +0300

    feat: add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     filename: 'main.js',
     publicPath: ''
   },
+  mode: "production",
   devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
     compress: true,
